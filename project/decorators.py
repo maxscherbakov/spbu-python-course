80cbe678028670998ca55eb18baf73972bcba146
maxscherbakov <max-tsh@yandex.ru>
2024-10-07 20:07:53 +0300

    add smart_args decorator and tests for smart_args

--- project/decorators.py
+++ project/decorators.py
@@ -1,23 +1,87 @@
 from functools import wraps
-from typing import Callable, Any
+from typing import Callable, Any, Tuple
+import inspect, copy, random
 
 
-def cache_decorator(max_results: Any = None) -> Callable[..., Any]:
-    def decorator(func: Callable[..., Any]) -> Callable[..., Any]:
-        cache: dict[Any, Any] = {}
+def cache_decorator(
+    func: Any = None, *, max_results: Any = None
+) -> Callable[..., Any]:
+    if func is None:
+        return lambda function: cache_decorator(
+            function, max_results=max_results
+        )
+    cache: dict[Any, Any] = {}
 
-        @wraps(func)
-        def wrapper(*args: Any, **kwargs: Any) -> Any:
-            if not max_results:
-                return func(*args, **kwargs)
-            key = (args, frozenset(kwargs.items()))
-            if key not in cache:
-                result = func(*args, **kwargs)
-                if len(cache) == max_results:
-                    cache.pop(next(iter(cache)))
-                cache[key] = result
-            return cache[key]
+    @wraps(func)
+    def wrapper(*args: Any, **kwargs: Any) -> Any:
+        if not max_results:
+            return func(*args, **kwargs)
+        key = (args, frozenset(kwargs.items()))
+        if key not in cache:
+            result = func(*args, **kwargs)
+            if len(cache) == max_results:
+                cache.pop(next(iter(cache)))
+            cache[key] = result
+        return cache[key]
 
-        return wrapper
+    return wrapper
 
-    return decorator
+
+class Evaluated:
+    def __init__(self, func: Any) -> None:
+        if isinstance(func, Isolated):
+            raise TypeError("You cannot combine Evaluated with Isolated.")
+        self.func = func
+
+    def execute(self) -> Any:
+        return self.func()
+
+
+class Isolated:
+    def __init__(self, value: Any = None):
+        if isinstance(value, Evaluated):
+            raise TypeError("You cannot combine Isolated with Evaluated.")
+
+
+def smart_args(
+    function: Any = None, position_args: bool = False
+) -> Callable[..., Any]:
+    if function is None:
+        return lambda func: smart_args(func, position_args=position_args)
+
+    def wrapper(*args: Any, **kwargs: Any) -> Any:
+        argspec = inspect.getfullargspec(function)
+        default_kwarg: dict[str, Any] | None = argspec.kwonlydefaults
+        if default_kwarg is not None:
+            for key, value in default_kwarg.items():
+                if key in kwargs:
+                    if isinstance(value, Isolated):
+                        kwargs[key] = copy.deepcopy(kwargs[key])
+                elif isinstance(value, Evaluated):
+                    kwargs[key] = value.execute()
+
+        if position_args:
+            default_args = argspec.defaults
+            all_positional_args = argspec.args
+            if default_args is not None and all_positional_args is not None:
+                list_args = list()
+                num_non_default_args = len(all_positional_args) - len(
+                    default_args
+                )
+                for i, arg in enumerate(args):
+                    if i < num_non_default_args or not isinstance(
+                        default_args[i - num_non_default_args], Isolated
+                    ):
+                        list_args.append(arg)
+                    else:
+                        list_args.append(copy.deepcopy(arg))
+                num_defined_default_args = len(args) - num_non_default_args
+                for i in range(num_defined_default_args, len(default_args)):
+                    if isinstance(default_args[i], Evaluated):
+                        list_args.append(default_args[i].execute())
+                    else:
+                        list_args.append(default_args[i])
+                return function(*list_args, **kwargs)
+        return function(*args, **kwargs)
+
+    return wrapper
--- tests/test_decorators.py
+++ tests/test_decorators.py
@@ -1,5 +1,7 @@
 import pytest
-from project.decorators import cache_decorator
+from project.decorators import cache_decorator, smart_args, Evaluated, Isolated
+from typing import Callable, Any
+import random
 
 
 class TestCacheDecorator:
@@ -14,5 +16,93 @@ class TestCacheDecorator:
         for i in range(0, 1000):
             fibonacci(i)
 
+        fibonacci(999)
+        fibonacci(998)
+        fibonacci(997)
         with pytest.raises(RecursionError):
             fibonacci(996)
+
+    def test_without_cache(self) -> None:
+        @cache_decorator
+        def fibonacci(n: int) -> int:
+            if n < 2:
+                return n
+            result: int = fibonacci(n - 1) + fibonacci(n - 2)
+            return result
+
+        with pytest.raises(RecursionError):
+            fibonacci(900)
+
+
+def get_random_number() -> int:
+    return random.randint(0, 100)
+
+
+class TestIsolatedEvaluated:
+    def test_combination_isolated_evaluated(self) -> None:
+        with pytest.raises(TypeError):
+            Isolated(Evaluated(get_random_number))
+            Evaluated(Isolated())
+
+
+class TestSmartArgs:
+    def test_only_kwargs_isolation(self) -> None:
+        @smart_args
+        def check_isolation(*, d: Any = Isolated()) -> Any:
+            d["a"] = 0
+            return d
+
+        no_mutable = {"a": 10}
+
+        assert check_isolation(d=no_mutable) == {"a": 0}
+        assert no_mutable == {"a": 10}
+
+    def test_only_kwargs_evaluated(self) -> None:
+        @smart_args
+        def check_evaluation(
+            *,
+            x: int = get_random_number(),
+            y: Any = Evaluated(get_random_number)
+        ) -> Any:
+            return x, y
+
+        assert check_evaluation()[0] == check_evaluation()[0]
+        assert check_evaluation()[1] != check_evaluation()[1]
+        assert check_evaluation(y=150)[1] == 150
+
+    def test_only_kwargs_isolated_evaluation(self) -> None:
+        @smart_args
+        def check_isolated_evaluation(
+            *, x: Any = Isolated(), y: Any = Evaluated(get_random_number)
+        ) -> Any:
+            x["a"] = 0
+            return x, y
+
+        no_mutable = {"a": 10}
+        res1 = check_isolated_evaluation(x=no_mutable)
+        res2 = check_isolated_evaluation(x=no_mutable, y=150)
+
+        assert res1[0] == {"a": 0}
+        assert no_mutable == {"a": 10}
+        assert res2[1] == 150
+
+    def test_with_position_args(self) -> None:
+        @smart_args(position_args=True)
+        def check_isolated_evaluation(
+            a: Any = Isolated(),
+            b: Any = Evaluated(get_random_number),
+            *,
+            x: Any = Isolated(),
+            y: Any = Evaluated(get_random_number)
+        ) -> Any:
+            x["a"] = 0
+            a["b"] = 1
+            return a, b, x, y
+
+        no_mutable = {"a": 10}
+        res1 = check_isolated_evaluation(no_mutable, x=no_mutable)
+        res2 = check_isolated_evaluation(no_mutable, 105, x=no_mutable)
+        assert res1[0] == {"a": 10, "b": 1}
+        assert no_mutable == {"a": 10}
+        assert 0 <= res1[1] < 100
+        assert res2[1] == 105
