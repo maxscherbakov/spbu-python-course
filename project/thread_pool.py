2003ad7c911ef9407d4e602024fa9e42dd6569e2
maxscherbakov <max-tsh@yandex.ru>
2024-10-22 20:48:37 +0300

    replace queue.Queue with list

--- project/thread_pool.py
+++ project/thread_pool.py
@@ -1,4 +1,3 @@
-from queue import Queue
 from typing import Callable, Any
 from threading import Thread, Condition
 
@@ -87,10 +86,10 @@ class ThreadPool:
         if num_threads <= 0:
             raise ValueError("The number of threads must be greater than 0")
         self.num_threads = num_threads
-        self._task_queue: Queue[TaskWrapper] = Queue()
+        self._task_queue: list[TaskWrapper] = []
         self._threads = []
         self._running = True
-        self._cv = Condition()
+        self._condition = Condition()
 
         for _ in range(num_threads):
             thread = Thread(target=self.thread_run)
@@ -105,12 +104,14 @@ class ThreadPool:
             task (Callable[..., Any]): the task that needs to be added to the queue.
             args (Any):
         """
-        if not self._running:
-            raise TypeError("The thread pool has been dispose")
-        task_wrap = TaskWrapper(task)
-        task_wrap.set_args(*args)
-        self._task_queue.put(task_wrap)
-        return task_wrap
+        with self._condition:
+            if not self._running:
+                raise TypeError("The thread pool has been dispose")
+            task_wrap = TaskWrapper(task)
+            task_wrap.set_args(*args)
+            self._task_queue.append(task_wrap)
+            self._condition.notify()
+            return task_wrap
 
     def thread_run(self) -> None:
         """
@@ -119,22 +120,21 @@ class ThreadPool:
         it executes tasks from the queue and terminates.
         """
         while True:
-            with self._cv:
-                self._cv.wait_for(
-                    lambda: self._task_queue.not_empty or not self._running
+            with self._condition:
+                self._condition.wait_for(
+                    lambda: self._task_queue or not self._running
                 )
-                if self._task_queue.empty() and self._running:
+                if not len(self._task_queue) and self._running:
                     continue
-                elif self._task_queue.empty() and not self._running:
+                if not len(self._task_queue) and not self._running:
                     break
-                elif self._task_queue.not_empty:
-                    task = self._task_queue.get()
+                task = self._task_queue.pop(0)
             task()
-            self._task_queue.task_done()
 
     def dispose(self) -> None:
         """Finishes accepting tasks and ends threads."""
-        self._running = False
-        self._task_queue.join()
+        with self._condition:
+            self._running = False
+            self._condition.notify_all()
         for i, thread in enumerate(self._threads):
             thread.join()
